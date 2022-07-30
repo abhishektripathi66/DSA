9c8a386d3c6e7e08d4166d900e10dc411fc8cf17
abhishektripathi66 <abhishekji66@gmail.com>
2022-07-30 17:56:28 +0530

    adding new interview questions

---
+++ ArrayInterviewQuestion/maxproduct.java
@@ -0,0 +1,33 @@
+package ArrayInterviewQuestion;
+
+/*
+ * How to find maximum product of two integers in the array where all elements are positive.
+
+Example
+
+int[] intArray = {10,20,30,40,50};
+maxProduct(intArray) // (40,50)
+ */
+public class maxproduct {
+    
+    public static void main(String[] args) {
+        maxproduct mp = new maxproduct();
+        mp.maxProduct(new int[]{10,20,30,40,50});
+    }
+
+    public String maxProduct(int[] intArray) {
+        // TODO
+        String out ="";
+        int prod =1;
+        for(int i=0;i<intArray.length;i++){
+            for(int j=i+1;j<intArray.length;j++){
+                if((intArray[i]*intArray[j])>prod){
+                 prod=intArray[i]*intArray[j];
+                 out = +intArray[i]+","+intArray[j];
+            }
+        }
+    }
+    return out;
+
+}
+}
