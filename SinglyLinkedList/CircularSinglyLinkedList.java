240fd671c1f7925a6bc4d4866c11713fa70c55a8
abhishektripathi66 <abhishekji66@gmail.com>
2022-08-29 07:19:15 +0530

    insertion in circular single list

--- SinglyLinkedList/CircularSinglyLinkedList.java
+++ SinglyLinkedList/CircularSinglyLinkedList.java
@@ -15,4 +15,39 @@ public class CircularSinglyLinkedList {
         size =1;
         return head;
      }
+
+     public void inserinLinkedlist(int nodevalue,int location){
+        Node node = new Node();
+        node.value = nodevalue;
+        if(head==null){
+            
+            createCircularSinglyLinkedList(nodevalue);
+            return;
+        }
+        else{
+            if(location == 0){
+                node.next = head;
+                head = node;
+                tail.next = head;
+            }
+            else if(location>=size){
+                node.next=head;
+                tail.next = node;
+                tail = node;
+            }
+            else{
+                Node tempNode = head;
+                int index=0;
+                while(index<location-1){
+                    tempNode = tempNode.next;
+                    index++;
+                }
+                Node nextnode = tempNode.next;
+                tempNode.next = node;
+                node.next = nextnode;
+    
+            }
+        }
+        size++;
+     }
 }
--- SinglyLinkedList/CircularSinglyLinkedListTest.java
+++ SinglyLinkedList/CircularSinglyLinkedListTest.java
@@ -4,7 +4,11 @@ public class CircularSinglyLinkedListTest {
     
     public static void main(String[] args) {
         var csll = new CircularSinglyLinkedList();
-        csll.createCircularSinglyLinkedList(10);
+        csll.createCircularSinglyLinkedList(9);
         System.out.println("First value "+csll.head.value + " Second value " +csll.head.next.value);
+        csll.inserinLinkedlist(10,0);
+        csll.inserinLinkedlist(11,2);
+        csll.inserinLinkedlist(12,2);
+        System.out.println("First value "+csll.head.value + " last value " +csll.tail.value);
     }
 }
