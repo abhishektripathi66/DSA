8f5ad6dcf447f942074cb05425428669268ef0f5
Abhishek Tripathi <42455093+abhishektripathi66@users.noreply.github.com>
2024-06-03 16:17:16 +0530

    Update DuplicatesMedium.java

--- Leetcode/DuplicatesMedium.java
+++ Leetcode/DuplicatesMedium.java
@@ -66,3 +66,19 @@ class DuplicatesMedium {
        return j; 
     }
 }
+
+/**
+two pointer approach
+
+**/
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (j == 1 || nums[i] != nums[j - 2]) {
+                nums[j++] = nums[i];
+            }
+        }
+        return j;
+    }
+}
