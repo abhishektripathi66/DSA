d0c42335c81f5ab0ebadc85332daa341b481a031
Abhishek Tripathi <42455093+abhishektripathi66@users.noreply.github.com>
2024-11-03 07:30:01 +0530

    Update RotateString.java

--- Leetcode/RotateString.java
+++ Leetcode/RotateString.java
@@ -30,3 +30,16 @@ class Solution {
             return ans;
     }
 }
+
+//fast solution
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length()!=goal.length())
+        return false;
+        String str=s+s;
+        if(str.contains(goal))
+        return true;
+        return false;
+    }
+
+}
