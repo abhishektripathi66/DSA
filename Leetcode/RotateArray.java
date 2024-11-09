ce4033441e909a1921c4de006beba02c3a3e5309
Abhishek Tripathi <42455093+abhishektripathi66@users.noreply.github.com>
2024-11-09 19:52:32 +0530

    Create RotateArray.java

---
+++ Leetcode/RotateArray.java
@@ -0,0 +1,70 @@
+/**
+189. Rotate Array
+Solved
+Medium
+Topics
+Companies
+Hint
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+ **/
+/**
+we use remainder.
+
+Input: nums = [1,2,3,4,5,6,7], k = 9
+To explain the algorithm first, it performs three reverse operations.
+
+First of all, we reverse all numbers.
+
+[1,2,3,4,5,6,7]
+↓
+[7,6,5,4,3,2,1]
+Then, we create two groups like solution 2. Before k and After k.
+
+     k
+[7,6,5,4,3,2,1]
+ # # * * * * *
+
+k is remainder
+Then, reverse all numbers in each group.
+
+     k
+[7,6,5,4,3,2,1]
+ # # * * * * *
+↓
+     k
+[6,7,1,2,3,4,5]
+ # # * * * * *
+[6,7,1,2,3,4,5] is actually answer.
+
+To summarize the algorithm, we use 3 reverse operation.
+
+reverse(0, last index) → whole array
+reverse(0, k - 1) → before k
+reverse(k, last index) → after k
+Let's see solution codes!
+
+Complexity
+Time complexity: O(n)
+Space complexity: O(1)
+
+  **/
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }    
+}
