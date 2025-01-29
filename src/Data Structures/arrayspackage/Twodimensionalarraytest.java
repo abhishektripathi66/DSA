f7c5de8d8e1d20f980e7dd022cd5524d1d3774f6
Abhishek Tripathi <42455093+abhishektripathi66@users.noreply.github.com>
2025-01-29 22:00:41 +0530

    Update and rename twodimensionalarraytest.java to Twodimensionalarraytest.java

--- src/Data Structures/arrayspackage/Twodimensionalarraytest.java
+++ src/Data Structures/arrayspackage/Twodimensionalarraytest.java
@@ -2,9 +2,9 @@ package arrayspackage;
 
 import java.util.Arrays;
 
-public class twodimensionalarraytest {
+public class Twodimensionalarraytest {
     public static void main(String[] args) {
-        twodimensionalarray tda = new twodimensionalarray(2, 2);
+        Twodimensionalarray tda = new Twodimensionalarray(2, 2);
         System.out.println(Arrays.deepToString(tda.arr));
         tda.insertelementinarray(0, 0, 0);
         tda.insertelementinarray(0, 1, 1);
