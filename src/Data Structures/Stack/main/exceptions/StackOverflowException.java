d2adcf767ab1cd98079f705fd26bedc360e58075
vukedd <vukedd@gmail.com>
2025-02-10 01:53:19 +0100

    Stack implementations and notes

---
+++ src/Data Structures/Stack/main/exceptions/StackOverflowException.java
@@ -0,0 +1,13 @@
+package main.exceptions;
+
+public class StackOverflowException extends Exception {
+	private String message;
+	
+	public StackOverflowException() {
+		message = "Stack overflow has occurred, max capacity is reached!";
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
