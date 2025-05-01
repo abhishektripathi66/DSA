c3bc15b6b40cb02e4e359d9264927bb6876b6d11
Priyank <152207361+priyankpriyank@users.noreply.github.com>
2025-05-01 17:14:14 -0400

    Added authorship description and example of an input array as well as the expected output

---
+++ src/Coding Questions/ArrayInterviewQuestion/slidingWindowMax.java
@@ -0,0 +1,12 @@
+
+// Author: Priyank Vora
+// Description: Find the maximum in each sliding window of size k in an array
+// Example: Input: [1,3,-1,-3,5,3,6,7], k = 3 => Output: [3,3,5,5,6,7]
+
+import java.util.*;
+
+public class slidingwindowmax {
+
+   
+    }
+
