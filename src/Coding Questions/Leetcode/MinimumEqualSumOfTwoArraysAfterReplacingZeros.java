2d30889ea73d68a8a328840fcb0ab57338dfd33b
Tien <95487001+tiennguyen2310@users.noreply.github.com>
2025-05-10 12:12:48 -0400

    Create MinimumEqualSumOfTwoArraysAfterReplacingZeros.java

---
+++ src/Coding Questions/Leetcode/MinimumEqualSumOfTwoArraysAfterReplacingZeros.java
@@ -0,0 +1,68 @@
+/*
+2918. Minimum Equal Sum of Two Arrays After Replacing Zeros
+Solved
+Medium
+Topics
+Companies
+Hint
+You are given two arrays nums1 and nums2 consisting of positive integers.
+
+You have to replace all the 0's in both arrays with strictly positive integers such that the sum of elements of both arrays becomes equal.
+
+Return the minimum equal sum you can obtain, or -1 if it is impossible.
+
+ 
+
+Example 1:
+
+Input: nums1 = [3,2,0,1,0], nums2 = [6,5,0]
+Output: 12
+Explanation: We can replace 0's in the following way:
+- Replace the two 0's in nums1 with the values 2 and 4. The resulting array is nums1 = [3,2,2,1,4].
+- Replace the 0 in nums2 with the value 1. The resulting array is nums2 = [6,5,1].
+Both arrays have an equal sum of 12. It can be shown that it is the minimum sum we can obtain.
+Example 2:
+
+Input: nums1 = [2,0,2,0], nums2 = [1,4]
+Output: -1
+Explanation: It is impossible to make the sum of both arrays equal.
+ 
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 105
+0 <= nums1[i], nums2[i] <= 106
+*/
+class Solution {
+    public long minSum(int[] nums1, int[] nums2) {
+        long sum1 = 0, sum2 = 0;
+        int cnt1 = 0, cnt2 = 0;
+        for(int i : nums1){
+            sum1 += i;
+            if(i == 0)
+                cnt1++;
+        }
+        for(int i : nums2){
+            sum2 += i;
+            if(i == 0)
+                cnt2++;
+        }
+
+        if(sum1 + cnt1 == sum2 + cnt2){
+            return sum1 + cnt1;
+        }
+                
+        if(sum1 + cnt1 < sum2 + cnt2){
+            //dif < 0
+            if(cnt1 > 0)
+                return sum2 + cnt2;
+            
+            return -1;
+        }
+
+        if(cnt2 > 0)
+            return sum1 + cnt1;
+        
+        return -1;
+    }
+}
