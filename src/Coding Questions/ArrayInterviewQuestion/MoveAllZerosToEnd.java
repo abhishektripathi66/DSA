db22b407d4fa9becaf9e957f7ded43c7d1143188
Abhishek Tripathi <42455093+abhishektripathi66@users.noreply.github.com>
2025-07-07 04:30:06 +0000

    adding the new file

---
+++ src/Coding Questions/ArrayInterviewQuestion/MoveAllZerosToEnd.java
@@ -0,0 +1,26 @@
+public class MoveAllZerosToEnd {
+    
+    public static void main(String[] args) {
+        int[] arr = new int[]{0, 1, 0, 3, 12, 0, 5};
+        moveZeroesToEnd(arr);
+        for(int i = 0; i < arr.length; i++) 
+            System.out.print(arr[i] + " ");
+    }
+
+    //time complexity: O(n)
+    //space complexity: O(1)
+    // This function moves all zeros in the array to the end while maintaining the order of non-zero elements.
+    public static int moveZeroesToEnd(int[] arr){
+            int j=0;
+            for(int i=0;i<arr.length;i++){
+                if(arr[i] != 0){
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i]=temp;
+                    j++;
+                }
+            }
+            
+            return j; // returns the count of non-zero elements
+    }
+}
---
+++ src/Coding Questions/ArrayInterviewQuestion/ReverseAnArray.java
@@ -0,0 +1,43 @@
+public class ReverseAnArray {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int[] reversedArr = reverseAnArray(arr);
+        if (reversedArr != null) {
+            for (int num : reversedArr) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        } else {
+            System.out.println("Array is null or empty");
+        }
+        arr = new int[]{1, 2, 3, 4};
+        reversedArr = reverseAnArray(arr);
+        if (reversedArr != null) {
+            for (int num : reversedArr) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        } else {
+            System.out.println("Array is null or empty");
+        }
+    }
+
+    // time complexity: O(n/2) - since we are swapping elements from both ends towards the center
+    // space complexity: O(1)
+    public static int[] reverseAnArray(int[] arr){
+        if(arr==null || arr.length==0){
+            return null;
+        }
+        int left=0,right=arr.length-1;
+        while(left<right){
+            int temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+            left++;
+            right--;
+        }
+        return arr;
+    }
+    
+}
