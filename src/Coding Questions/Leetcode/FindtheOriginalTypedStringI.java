26bbafeeb710d30d20f1a0e7fcc1391f14db2f02
Dhruv Patel <dhruvpatel19201@gmail.com>
2025-07-10 23:23:16 -0400

    BFS and DFS traversals of a given unweighted graph

--- src/Coding Questions/Leetcode/FindtheOriginalTypedStringI.java
+++ src/Coding Questions/Leetcode/FindtheOriginalTypedStringI.java
@@ -1,20 +1,61 @@
-/**
-700. Search in a Binary Search Tree
+/*
+3330. Find the Original Typed String I
+Solved
 Easy
 Topics
+premium lock icon
 Companies
-You are given the root of a binary search tree (BST) and an integer val.
+Hint
+Alice is attempting to type a specific string on her computer. However, she tends to be clumsy and may press a key for too long, resulting in a character being typed multiple times.
 
-Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
-  **/
-public class FindtheOriginalTypedStringI {
+Although Alice tried to focus on her typing, she is aware that she may still have done this at most once.
+
+You are given a string word, which represents the final output displayed on Alice's screen.
+
+Return the total number of possible original strings that Alice might have intended to type.
+
+ 
+
+Example 1:
+
+Input: word = "abbcccc"
+
+Output: 5
+
+Explanation:
+
+The possible strings are: "abbcccc", "abbccc", "abbcc", "abbc", and "abcccc".
+
+Example 2:
+
+Input: word = "abcd"
+
+Output: 1
+
+Explanation:
+
+The only possible string is "abcd".
+
+Example 3:
+
+Input: word = "aaaa"
+
+Output: 4
+
+ 
+
+Constraints:
+
+1 <= word.length <= 100
+word consists only of lowercase English letters.
+*/
+public class FindTheOriginalTypedStringI {
     public int possibleStringCount(String word) {
-        int count=1;
-        for(int i=1;i<word.length();i++){
-            if(word.charAt(i)==word.charAt(i-1)){
-                count++;
-            }
+        int res = 1;
+        for(int i = 1; i < word.length(); i++){
+            if(word.charAt(i - 1) == word.charAt(i))
+                res++;
         }
-        return count;
+        return res;
     }
 }
