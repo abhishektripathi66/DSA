8f045339ea7a28d7253a3590df9405f082f83c4f
Abhishek Tripathi <42455093+abhishektripathi66@users.noreply.github.com>
2025-07-13 16:53:05 +0000

    complete array questions

--- src/Coding Questions/ArrayInterviewQuestion/Equilibrium.java
+++ src/Coding Questions/ArrayInterviewQuestion/Equilibrium.java
@@ -3,8 +3,9 @@ package ArrayInterviewQuestion;
 public class Equilibrium {
     
     public static void main(String[] args) {
-        int[] arr = {1, 3, 5, 2, 2};
+        int[] arr = {2, 3, 5, 2, 3};
         System.out.println(findEquilibriumIndex(arr)); // Output: 3
+        System.out.println(threePartPossible(arr)); // Output: 3
     }
 
     public static boolean findEquilibriumIndex(int[] arr) {
@@ -22,5 +23,25 @@ public class Equilibrium {
         }
 
         return false; // No equilibrium index found
+    }
+
+    public static boolean threePartPossible(int[] arr){
+        int totalsum=0;
+        for(int num:arr){
+            totalsum+=num;
+        }
+        if(totalsum%3!=0) return false;
+        int partsum = totalsum/3;
+        int count = 0;
+        int currentSum = 0;
+        for(int i=0;i<arr.length;i++){
+            currentSum+=arr[i];
+            if(currentSum == partsum) {
+                count++;
+                currentSum = 0; // Reset current sum for the next part  
+            }
+        }
+        if(count==3) return true; // Found three parts with equal sum
+        return false; // Not possible to divide into three parts with equal sum
     }
 }
