f6dd41c59e009dab31311c95d1c31ddb706bdb92
prabhat <prabhattripathi20@gmail.com>
2025-07-16 21:56:18 +0530

    update the java files

--- src/codingquestions/leetcode/ContainsDuplicateII.java
+++ src/codingquestions/leetcode/ContainsDuplicateII.java
@@ -1,4 +1,4 @@
-
+package codingquestions.leetcode;
 /*Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
 Example 1:
 Input: nums = [1,2,3,1], k = 3
@@ -14,7 +14,10 @@ Output: false
 */
 
 //Code:
-class Solution {
+
+import java.util.HashMap;
+
+public class ContainsDuplicateII {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         HashMap<Integer,Integer> s = new HashMap<>();
         for(int i=0;i<nums.length;i++){
