e7794d506cb935e749586d05f76bb4ae3dc75168
Abhishek Tripathi <abhishekji66@gmail.com>
2025-07-23 16:15:43 +0000

    effificientsolution

--- src/codingquestions/leetcode/MaximumScoreFromRemovingSubstrings.java
+++ src/codingquestions/leetcode/MaximumScoreFromRemovingSubstrings.java
@@ -79,4 +79,41 @@ public class MaximumScoreFromRemovingSubstrings {
 
             return score;
     }
+
+    //fast solution
+    public int maximumGain1(String s, int x, int y) {
+        int res = 0;
+        char[] letters = s.toCharArray();
+        int n = letters.length;
+        char a = 'a', b = 'b';
+        int aCnt = 0, bCnt = 0;
+        if (x < y) {
+            int tmp = x;
+            x = y;
+            y = tmp;
+            a = 'b';
+            b = 'a';
+        }
+        for (int i = 0; i < n; i++) {
+            if (letters[i] == a) {
+                aCnt++;
+            } else if (letters[i] == b) {
+                if (aCnt > 0) {
+                    aCnt--;
+                    res += x;
+                } else {
+                    bCnt++;
+                }
+            } else {
+                res += Math.min(aCnt, bCnt) * y;
+                aCnt = 0;
+                bCnt = 0;
+            }
+        }
+        if (aCnt!= 0) {
+            res += Math.min(aCnt, bCnt) * y;
+        }
+
+        return res;
+    }
 }
