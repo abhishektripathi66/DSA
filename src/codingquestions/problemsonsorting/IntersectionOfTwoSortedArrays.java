5f5f3891a40b4c85c39804952c3527410d80f468
Abhishek Tripathi <abhishekji66@gmail.com>
2025-07-30 11:01:50 +0000

    Find Uninon using the merge sort

--- src/codingquestions/problemsonsorting/IntersectionOfTwoSortedArrays.java
+++ src/codingquestions/problemsonsorting/IntersectionOfTwoSortedArrays.java
@@ -5,6 +5,7 @@ public class IntersectionOfTwoSortedArrays {
         int[] arr1= {10,20,20,40,60};
         int[] arr2 = {2,20,20,20};
         findIntersection(arr1, arr2);
+        findUnion(arr1, arr2);
 
     }
 
@@ -20,6 +21,30 @@ public class IntersectionOfTwoSortedArrays {
             else if(arr1[i]>arr2[j]) j++;
             else i++;
 
+        }
+    }
+
+    public static void findUnion(int[] arr1, int[] arr2){
+        int i=0,j=0;
+        while(i<arr1.length && j<arr2.length){
+            if(i>0 && arr1[i]==arr1[i-1]){i++; continue;}
+            if(j>0 && arr2[j]==arr1[j-1]){j++; continue;}
+            if(arr1[i]<arr2[j]){
+                    System.out.println(arr1[i++]);
+            } 
+            else if(arr1[i]>arr2[j]){
+                System.out.println(arr2[j++]);
+            }
+            else{
+                System.out.println(arr1[i++]); j++;
+            }
+
+        }
+        while(i<arr1.length){
+            System.out.println(arr1[i++]);
+        }
+        while(j<arr2.length){
+            System.out.println(arr2[j++]);
         }
     }
 }
