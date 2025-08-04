71942a0c0149000f2163bbd055c6ff42115a3a21
Abhishek Tripathi <abhishekji66@gmail.com>
2025-08-04 04:51:55 +0000

    adding the method for the HoarePartition

--- src/algorithms/sortingalgorithms/quicksort/ParitionFunction.java
+++ src/algorithms/sortingalgorithms/quicksort/ParitionFunction.java
@@ -8,13 +8,13 @@ public class ParitionFunction {
         int[] arr ={5,13,6,9,12,11,8};
         partitionNaive(arr, 0, 6, 6); // p means all the elements less than the element at that index should be placed at left and greater elements at right
         System.out.println(Arrays.toString(arr));
-        LumtoPartition(arr, 0, 6);
+        LumutoPartition(arr, 0, 6);
         System.out.println(Arrays.toString(arr));
         int[] arr1 = {70,60,80,40,30};
-        LumtoPartition(arr1, 0, 4);
+        LumutoPartition(arr1, 0, 4);
         System.out.println(Arrays.toString(arr1));
         int[] arr2 = {30,40,25,50,80};
-        LumtoPartition(arr2, 0, 4);
+        LumutoPartition(arr2, 0, 4);
         System.out.println(Arrays.toString(arr2));
         // when pivot is not the last element in the lomuto partition we would then swap it with the last element
     }
@@ -36,7 +36,8 @@ public class ParitionFunction {
     }
 
     // in this we partitioning method we would be assuming that the pivot is always at the last index i.e, h.
-    public static void LumtoPartition(int[] arr, int low, int high){
+    // time complexoty O(n) O(1) space complexity
+    public static void LumutoPartition(int[] arr, int low, int high){
             int pivot = arr[high];
             int i=low-1;
             for(int j=low;j<=high-1;j++){
@@ -54,7 +55,7 @@ public class ParitionFunction {
     }
 
     public static void HoarePartition(int arr[], int l,int h){
-        
+            
     }
     
 }
