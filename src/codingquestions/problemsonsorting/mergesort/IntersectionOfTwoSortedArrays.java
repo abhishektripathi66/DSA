12bbbd572bd1d0fc33de678f2c64849f98a270bd
Abhishek Tripathi <42455093+abhishektripathi66@users.noreply.github.com>
2025-08-08 16:39:13 +0000

    updating the paths

--- src/codingquestions/problemsonsorting/mergesort/IntersectionOfTwoSortedArrays.java
+++ src/codingquestions/problemsonsorting/mergesort/IntersectionOfTwoSortedArrays.java
@@ -1,4 +1,4 @@
-package codingquestions.problemsonsorting;
+package codingquestions.problemsonsorting.mergesort;
 
 public class IntersectionOfTwoSortedArrays {
     public static void main(String[] args) {
