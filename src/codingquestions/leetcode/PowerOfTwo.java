08bbc90f01b91005c87d9d154d35e0ac9be4ef53
Abhishek Tripathi <42455093+abhishektripathi66@users.noreply.github.com>
2025-08-09 21:03:19 +0530

    Update PowerOfTwo.java

--- src/codingquestions/leetcode/PowerOfTwo.java
+++ src/codingquestions/leetcode/PowerOfTwo.java
@@ -34,11 +34,15 @@ Constraints:
 
 Follow up: Could you solve it without loops/recursion?
 */
-class Solution {
+public class PowerOfTwo {
     public boolean isPowerOfTwo(int n) {
         if(n <= 1)
             return (n == 1);
         
         return (((n & 1) == 0) && isPowerOfTwo(n >> 1));
+    }
+
+    public boolean isPowerOfTwoEff(int n) {
+        return n>0 && (n&(n-1))==0;
     }
 }
