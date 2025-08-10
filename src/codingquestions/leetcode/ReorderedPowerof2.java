eb42236649b425dc5285bce6498bd1f64ecce555
Abhishek Tripathi <42455093+abhishektripathi66@users.noreply.github.com>
2025-08-10 19:23:35 +0530

    Create ReorderedPowerof2.java

---
+++ src/codingquestions/leetcode/ReorderedPowerof2.java
@@ -0,0 +1,58 @@
+/**
+869. Reordered Power of 2
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+You are given an integer n. We reorder the digits in any order (including the original order) such that the leading digit is not zero.
+
+Return true if and only if we can do this so that the resulting number is a power of two.
+
+ 
+
+Example 1:
+
+Input: n = 1
+Output: true
+Example 2:
+
+Input: n = 10
+Output: false
+ 
+
+Constraints:
+
+1 <= n <= 109
+**/
+
+public class ReorderedPowerof2 {
+    public boolean reorderedPowerOf2(int n) {
+        //creating an array to hold the count of the digits in n
+        int[] count = new int[10];
+        while(n>0){
+            count[n%10]++;
+            n/=10;
+        }
+        // with the constraints provided we can see that the max power can be of 31, so we calulate the digits for each power and compare with the 
+        for(int i=0;i<31;i++){
+            int power = 1<<i;
+            int[] count1 = new int[10];
+            int k=power;
+            while(k>0){
+            count1[k%10]++;
+            k/=10;
+            }
+            if(isequal(count1,count)) return true;
+        }
+        return false;
+        }
+  
+        // check if both the arrays have equal values or not
+        public static boolean isequal(int[] arr1, int[] arr2){
+            for(int i=0;i<10;i++){
+                if(arr1[i]!=arr2[i]) return false;
+            }
+            return true;
+        }
+    }
