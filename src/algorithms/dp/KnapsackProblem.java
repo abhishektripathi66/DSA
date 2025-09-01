bb04971487ad7c44ec19a51b407771db862bc96e
Dhruv Patel <dhruvpatel19201@gmail.com>
2025-08-31 22:36:19 -0400

    0/1 knapsack using tabulation method

--- src/algorithms/dp/KnapsackProblem.java
+++ src/algorithms/dp/KnapsackProblem.java
@@ -32,4 +32,28 @@ public class KnapsackProblem {
 
         return memo[n][W] = Math.max(pick, notPick);
     }
+
+    private static int knapsackProblemTabulation(int W, int wt[], int val[], int n){
+
+         int[][] dp = new int[n+1][W+1];
+
+         for(int i=0; i<=n; i++)
+            dp[i][0] = 0;
+        
+        for(int j=0; j<=W; j++)
+            dp[0][j] = 0;
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=W; j++){
+
+                if(wt[i-1] > j)
+                    dp[i][j] = dp[i-1][j]; // can’t pick item
+
+                else 
+                    dp[i][j] = Math.max(val[i-1] + dp[i-1][j - wt[i-1]], dp[i-1][j]);
+            }
+        }
+
+        return dp[n][W];
+     }
 }
