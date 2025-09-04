ff39743f58b4197e351ef59e248ee0d853177f79
Dhruv Patel <dhruvpatel19201@gmail.com>
2025-09-04 00:38:03 -0400

    Min coins using tabulation method

--- src/algorithms/dp/MinCoins.java
+++ src/algorithms/dp/MinCoins.java
@@ -14,6 +14,7 @@ public class MinCoins {
             Arrays.fill(row, -1);
 
         System.out.println("Min coins required: "+minCoinsMemo(0, sum, coins, memo));
+        System.out.println("Min coins required: "+minCoinsTabulation(sum, coins, n));
     }
 
     private static int minCoinsMemo(int i, int sum, int[] coins, int[][] memo) {
@@ -35,9 +36,38 @@ public class MinCoins {
             if (take != Integer.MAX_VALUE) take++;
         }
       
-
         int noTake = minCoinsMemo(i + 1, sum, coins, memo);
         
         return memo[i][sum] = Math.min(take, noTake);
+    }
+
+
+    private static int minCoinsTabulation(int sum, int[] coins, int n){
+        int[][] dp = new int[n+1][sum+1];
+
+        // Base case: sum = 0 requires 0 coins
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+
+        // Base case: with no coins, we can't form positive sums
+        for (int s = 1; s <= sum; s++) {
+            dp[n][s] = Integer.MAX_VALUE;
+        }
+
+        // Fill table bottom-up
+        for (int i = n-1; i >= 0; i--) {
+            for (int s = 1; s <= sum; s++) {
+                int take = Integer.MAX_VALUE;
+                if (coins[i] <= s && dp[i][s - coins[i]] != Integer.MAX_VALUE) {
+                    take = 1 + dp[i][s - coins[i]];
+                }
+                int noTake = dp[i+1][s];
+                dp[i][s] = Math.min(take, noTake);
+            }
+        }
+
+        return dp[0][sum] == Integer.MAX_VALUE ? -1 : dp[0][sum];
+
     }
 }
