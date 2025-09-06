edd7182474e194df5dc41e73bba8fba6ea85eb46
Dhruv Patel <dhruvpatel19201@gmail.com>
2025-09-05 22:18:56 -0400

    added required comments for different steps and introduction

--- src/algorithms/dp/MinCoins.java
+++ src/algorithms/dp/MinCoins.java
@@ -2,6 +2,40 @@ package algorithms.dp;
 
 import java.util.Arrays;
 
+/*
+ * Minimum Coin Change Problem using Memoization and Tabulation
+ *
+ * Problem:
+ *   Given an array of coin denominations and a target sum,
+ *   find the minimum number of coins required to make the sum.
+ *   Each coin can be used an unlimited number of times.
+ *   If it is not possible to form the sum, return -1.
+ *
+ * Approach:
+ *   - Memoization (Top-Down):
+ *       Recursively decide whether to include or exclude a coin
+ *       and store results in a memoization table to avoid recomputation.
+ *   - Tabulation (Bottom-Up):
+ *       Build a DP table iteratively where dp[i][s] stores the minimum
+ *       number of coins required to make sum 's' using coins[i..n-1].
+ *
+ * Time Complexity:
+ *   - O(n * sum), where n is the number of coins.
+ *
+ * Space Complexity:
+ *   - O(n * sum) for the DP table or memoization matrix.
+ *   - Can be optimized to O(sum) using a 1D DP array.
+ *
+ * Example:
+ *   Input:
+ *     coins = {3, 4, 1}
+ *     sum   = 5
+ *
+ *   Output:
+ *     Min coins required: 2
+ *     Explanation: Either {4, 1} or {3, 1, 1}
+ */
+
 public class MinCoins {
     public static void main(String[] args) {
 
@@ -31,13 +65,15 @@ public class MinCoins {
         
         int take = Integer.MAX_VALUE;
         
-        if (coins[i] <= sum) {
+        if (coins[i] <= sum) { //consider opiton if coin value is less than or equal to curr sum
             take = minCoinsMemo(i, sum - coins[i], coins, memo);
             if (take != Integer.MAX_VALUE) take++;
         }
-      
+
+        //skip curr coin
         int noTake = minCoinsMemo(i + 1, sum, coins, memo);
         
+        //choose min from both options
         return memo[i][sum] = Math.min(take, noTake);
     }
 
@@ -45,17 +81,17 @@ public class MinCoins {
     private static int minCoinsTabulation(int sum, int[] coins, int n){
         int[][] dp = new int[n+1][sum+1];
 
-        // Base case: sum = 0 requires 0 coins
+        //sum = 0 requires 0 coins
         for (int i = 0; i <= n; i++) {
             dp[i][0] = 0;
         }
 
-        // Base case: with no coins, we can't form positive sums
+        //with no coins, we can't form positive sums
         for (int s = 1; s <= sum; s++) {
             dp[n][s] = Integer.MAX_VALUE;
         }
 
-        // Fill table bottom-up
+        //fill table bottom-up
         for (int i = n-1; i >= 0; i--) {
             for (int s = 1; s <= sum; s++) {
                 int take = Integer.MAX_VALUE;
