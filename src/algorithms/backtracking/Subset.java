94bcf34cb7d3ea36a0997f4f56c683c98ac9ca5a
tharunpakash <tharunprakash999@gmail.com>
2025-10-09 09:56:53 -0700

    subset question added.

---
+++ src/algorithms/backtracking/Subset.java
@@ -0,0 +1,30 @@
+
+class Subset{
+    public static void main(String[]args){
+        int[]nums={1,2,3};
+        System.out.println(subsets(nums));
+    }
+  static public List<List<Integer>> subsets(int[] nums) {
+         Arrays.sort(nums);
+        List<List<Integer>> outer=new ArrayList<>();
+        List<Integer> internal=new ArrayList<>();
+
+        int end=0;
+        outer.add(internal);
+        for(int i=0;i<nums.length;i++){
+                    int start=0;
+if(i>0&&nums[i-1]==nums[i]){
+  start=end+1;  
+}
+end=outer.size()-1;
+int n=outer.size();
+for(int j=start;j<n;j++){
+    internal=new ArrayList<>(outer.get(j));
+    internal.add(nums[i]);
+    outer.add(internal);
+}
+
+        }
+        return outer;
+    }
+}
