c2b95710159d3f1e2fff8e1889a26424e5d0c241
Dhruv Patel <dhruvpatel19201@gmail.com>
2025-11-05 22:49:42 -0500

    Binary search and greedy check for allocate minimum pages problem

--- src/algorithms/dp/AllocateMinimumPages.java
+++ src/algorithms/dp/AllocateMinimumPages.java
@@ -15,9 +15,11 @@ public class AllocateMinimumPages {
         System.out.println("Minimized maximum pages : "+allocateMinimumPagesMemo(arr, n, k, memo));
 
         System.out.println("Minimized maximum pages : "+allocateMinimumPagesTabulation(arr, n, k));
+
+        System.out.println("Minimized maximum pages using binary search : "+allocateMinimumPagesBinarySearch(arr, n, k));
     }
     
-    //Time complexity: O(2^n)
+    //Time complexity: Exponential (≈ O(k × 2^n))
     private static int allocateMinimumPagesRec(int[] arr, int n, int k){
 
         //all books go to one student
@@ -66,6 +68,7 @@ public class AllocateMinimumPages {
         return memo[n][k] = res;
     }
 
+    //Time complexity: O(k * n^3) -> Could be O(k * n^2) if we use prefix sum instead calling sum everytime
     private static int allocateMinimumPagesTabulation(int[] arr, int n, int k){
         
         int[][] dp = new int[k+1][n+1]; //reverse ordering compared to memoization table
@@ -90,6 +93,51 @@ public class AllocateMinimumPages {
         }
 
         return dp[k][n];
+    }
+
+    //Time Complexity: O(nlog(sum))
+    //allocation should be continous, okay if given array is not sorted
+    private static int allocateMinimumPagesBinarySearch(int[] arr, int n, int k){
+        int sum = 0, max = 0;
+        for(int page : arr){
+            max = Math.max(max, page);
+            sum += page;
+        }
+
+        int low = max, high = sum, res = -1;
+
+        while(low <= high){
+            int mid = (low + high)/2;
+
+            if(isFeasible(arr, n, k, mid)){
+                res = mid;
+                high = mid - 1;
+            }
+            else
+                low = mid + 1;
+
+        }
+        return res;
+    }
+
+    //doesn’t explore all possibilities like recursion or dp, instead it makes a greedy choice
+    private static boolean isFeasible(int[] arr, int n, int k, int ans){
+
+        int req = 1, sum = 0;
+
+        for(int i=0; i<n; i++){
+             if(arr[i] > ans) // single book exceeds limit
+                return false;
+
+            if(sum + arr[i] > ans){ //when about to exceed
+                sum = arr[i];
+                req++;
+            }
+            else
+                sum += arr[i];
+        }
+
+        return req <= k;
     }
 
     //utility method to count number of pages in given range
