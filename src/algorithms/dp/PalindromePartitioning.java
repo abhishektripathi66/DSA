e4a42bdae8def19a48f9c6b47ba2f014e55a210e
Dhruv Patel <dhruvpatel19201@gmail.com>
2025-11-14 22:17:59 -0500

    Palindrom partitioning problem using memoization

--- src/algorithms/dp/PalindromePartitioning.java
+++ src/algorithms/dp/PalindromePartitioning.java
@@ -1,5 +1,7 @@
 package algorithms.dp;
 
+import java.util.Arrays;
+
 public class PalindromePartitioning {
 
     //minimum number of cuts required to make all strings palindrome
@@ -7,10 +9,17 @@ public class PalindromePartitioning {
         String s = "abbac";
         int n = s.length();
         System.out.println("Mimum cuts required : "+isPalindromeRec(s, 0, n-1));
+
+        int[][] memo = new int[n+1][n+1];
+        for(int[] row : memo)
+            Arrays.fill(row, -1);
+        System.out.println("Mimum cuts required : "+isPalindromeMemo(s, 0, n-1, memo));
+
     }
     
     private static int isPalindromeRec(String s, int i, int j){
 
+        //Base case
         if(i>=j || isPalindrome(s, i, j))
             return 0;
         
@@ -23,6 +32,26 @@ public class PalindromePartitioning {
 
         return res;
     }
+
+    private static int isPalindromeMemo(String s, int i, int j, int[][] memo){
+        //Base case
+        if(i>=j || isPalindrome(s, i, j))
+            return 0;
+        
+        //return result if already computed
+        if(memo[i][j] != -1)
+            return memo[i][j];
+        
+        int res = Integer.MAX_VALUE;
+        for(int k=i; k<j; k++){
+            //take the minimum value from all cuts
+            res = Math.min(res, 1 + isPalindromeMemo(s, i, k, memo) + isPalindromeMemo(s, k+1, j, memo));
+        }
+
+        //store and return result
+        return memo[i][j] = res;
+    }
+
     private static boolean isPalindrome(String s, int i, int j){
         while(i < j){
             if(s.charAt(i) == s.charAt(j)){
