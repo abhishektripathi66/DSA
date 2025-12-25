b0dba731c64a936842c73b8b7c202917032d88e6
Dhruv Patel <dhruvpatel19201@gmail.com>
2025-12-25 04:00:56 -0500

    Initial structure for a segment tree class

---
+++ src/datastructures/segmenttree/SegmentTreeAnother.java
@@ -0,0 +1,10 @@
+package datastructures.segmenttree;
+
+//useful for problems involving range queries, such as finding the sum, minimum, maximum, or any other operation over a specific range of elements in an array
+//it is built recursively, until each segment represents a one element
+//fast query and update operations with a time complexity of O(log n)
+public class SegmentTreeAnother {
+    public static void main(String[] args) {
+        
+    }
+}
