4f49888b7e1ae99afd725ff261754a70a95ff4bf
Tejasguntiwar2 <guntiwar.tejas@gmail.com>
2026-01-21 01:28:38 +0530

    Improve BubbleSort readability with comments and JavaDoc

--- src/algorithms/sortingalgorithms/bubblesort/BubbleSort.java
+++ src/algorithms/sortingalgorithms/bubblesort/BubbleSort.java
@@ -2,31 +2,48 @@ package algorithms.sortingalgorithms.bubblesort;
 
 /**
  * Bubble Sort Algorithm
- * ----------------------
- * Repeatedly steps through the list, compares adjacent elements,
- * and swaps them if they are in the wrong order.
- * <p>
- * Time Complexity: O(n^2)
- * Space Complexity: O(1)
- * <p>
- * Author: Shubhanjay Tiwari
+ *
+ * This class provides an implementation of the Bubble Sort algorithm,
+ * which repeatedly compares adjacent elements and swaps them if they
+ * are in the wrong order.
+ *
+ * Time Complexity:
+ * - Best Case: O(n)
+ * - Average Case: O(n^2)
+ * - Worst Case: O(n^2)
+ *
+ * Space Complexity:
+ * - O(1) (In-place sorting)
+ * * Author: Tejas Guntiwar
  */
 public class BubbleSort {
-    public static void bubbleSort(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+
+    /**
+     * Sorts the given array using Bubble Sort.
+     *
+     * @param arr the array to be sorted
+     */
+    static void bubbleSort(int[] arr) {
+        int length = arr.length;
+
+        // Traverse through all array elements
+        for (int pass = 0; pass < length; pass++) {
+
+            // Last pass elements are already in correct position
+            for (int index = 0; index < length - pass - 1; index++) {
+
+                // Swap adjacent elements if they are in wrong order
+                if (arr[index] > arr[index + 1]) {
+                    int temp = arr[index];
+                    arr[index] = arr[index + 1];
+                    arr[index + 1] = temp;
                 }
             }
         }
     }
 
     public static void main(String[] args) {
-        int[] arr = {5, 8, 9, 1, 2, 6, 4};
+        int[] arr = { 5, 8, 9, 1, 2, 6, 4 };
         System.out.println("Before Bubble Sort:");
         printArray(arr);
 
