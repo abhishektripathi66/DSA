9abf9fca724e89ead5ec54445c994da2c00bea52
Dhruv Patel <dhruvpatel19201@gmail.com>
2026-01-29 02:09:48 -0500

    implemented insert operation in binary search tree

--- src/algorithms/binarytree/BinarySearchTree.java
+++ src/algorithms/binarytree/BinarySearchTree.java
@@ -27,7 +27,9 @@ public class BinarySearchTree {
 
         System.out.println("For x=10 : "+searchRec(root, 10));
         System.out.println("For x=12 : "+searchIterative(root, 12));
-
+        
+        //reassign the root, otherwise changes won't reflect
+        root = insertRec(root, 3);
 
     }
 
@@ -53,6 +55,43 @@ public class BinarySearchTree {
                 root = root.left;
         }
         return false;
+    }
+
+    private static Node insertRec(Node root, int x){
+        if(root == null)
+            return new Node(x);
+        
+        if(root.key > x)
+            root.left = insertRec(root.left, x);
+        else if(root.key < x)
+            root.right = insertRec(root.right, x);
+
+        return root;
+    }
+
+    private static Node insertIterative(Node root, int x){
+        Node temp = new Node(x);
+        Node parent = null, curr = root;
+
+        while(curr != null){
+            parent = curr;
+
+            if(curr.key > x)
+                curr = curr.left;
+            else if(curr.key < x)
+                curr = curr.right;
+            else
+                return root;
+        }
+
+        if(parent == null)
+            return temp;
+        else if(parent.key > x)
+            parent.left = temp;
+        else
+            parent.right = temp;
+
+        return root;
     }
 }
 
