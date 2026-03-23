c03e000d4b64e78ac4eb3142d69059b9ece16b45
Dhruv Patel <dhruvpatel19201@gmail.com>
2026-03-22 21:14:22 -0400

    Heap initial structure and insert method

---
+++ src/datastructures/heap/MinHeap.java
@@ -0,0 +1,65 @@
+package datastructures.heap;
+
+public class MinHeap {
+
+    private int[] arr;
+    private int size;
+    private int capacity;
+
+    MinHeap(int c){
+        arr = new int[c];
+        size = 0;
+        capacity = c;
+    }
+
+
+    public static void main(String[] args) {
+        MinHeap minHeap = new MinHeap(6);
+        minHeap.insert(10);
+        minHeap.insert(30);
+        minHeap.insert(50);
+        minHeap.insert(20);
+        minHeap.insert(60);
+        minHeap.insert(40);
+
+        minHeap.printHeap();
+    }
+    
+    private int left(int i){
+        return 2*i + 1;
+    }
+
+    private int right(int i){
+        return 2*i + 2;
+    }
+
+    private int parent(int i){
+        return (i-1)/2;
+    }
+
+
+    private void insert(int x){
+        if(size == capacity)
+            return;
+        size++;
+        arr[size-1] = x;
+
+        for(int i=size-1; i!=0 && arr[parent(i)] > arr[i]; ){
+            swap(i, parent(i));
+            i = parent(i);
+        }
+    }
+
+    private void printHeap() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    private void swap(int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
